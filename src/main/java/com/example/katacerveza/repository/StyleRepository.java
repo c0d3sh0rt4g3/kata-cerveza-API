9a8ce07a972993d996044e57f1033d450d21474e
out0f4mm0 <eriking2004@gmail.com>
2023-12-04 09:16:03 +0100

    Done StyleRepository

---
+++ src/main/java/com/example/katacerveza/repository/StyleRepository.java
@@ -0,0 +1,6 @@
+package com.example.katacerveza.repository;
+
+import com.example.katacerveza.model.Style;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StyleRepository extends JpaRepository<Style, Integer> {}
