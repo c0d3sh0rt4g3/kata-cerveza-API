96f733cfd213e987cb1df1da088ddeeef513cd2a
out0f4mm0 <eriking2004@gmail.com>
2023-12-04 09:04:06 +0100

    Done CategoryRepository

---
+++ src/main/java/com/example/katacerveza/repository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.katacerveza.repository;
+
+import com.example.katacerveza.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {}
